8a5959cbb5dcab3f157282a6eadceb13ffee1720
Erika Ranjo <erikaranjo05@gmail.com>
2025-04-06 10:51:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('userIcon').addEventListener('click', function() {
-    // You can redirect to either login or signup
-    window.location.href = 'login.html'; // Or 'signup.html'
+    // redirect to login or signup
+    window.location.href = 'login.html';
   });
