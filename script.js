125dfe8c6f05e72c7245935306899184242633c4
CrtlCAlt <erikaranjo05@gmail.com>
2025-04-06 10:49:45 +0800

    Login connection, db connection

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('userIcon').addEventListener('click', function() {
+    // You can redirect to either login or signup
+    window.location.href = 'login.html'; // Or 'signup.html'
+  });
--- server.js
+++ server.js
@@ -5,6 +5,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
  
+const mysql = require('mysql2');
+
+// MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'trendora'
+});
+
+// Connect to DB
+db.connect(err => {
+    if (err) {
+        console.error('Database connection failed: ', err);
+        return;
+    }
+    console.log('Connected to MySQL database.');
+});
+
 app.get('/', (req, res) => {
     res.send('Server is running...');
 });
